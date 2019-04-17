cb96983d2d049eff1b2edeb55494f527002b15d0
Jessica Brumfield <jessicabrumfield@Jessicas-MacBook-Pro.local>
2019-04-17 11:57:49 -0500

    entry class created

--- mod3-project-gratitude-journal/gratitudeFrontEnd/src/entry.js
+++ mod3-project-gratitude-journal/gratitudeFrontEnd/src/entry.js
@@ -1,8 +1,8 @@
- class gratitude {
+ class Entry {
      constructor (value1,value2,value3){
          this.div = document.createElement('div')
-         this.div.setAttribute('class','journal-entry')
-         const entry = document.createElement('ol')
+         this.div.setAttribute('class','card')
+         const content = document.createElement('ol')
          const answer1 = document.createElement('li')
          const answer2 = document.createElement('li')
          const answer3 = document.createElement('li')
@@ -13,6 +13,9 @@
          answer2.innerText = value2
          answer3.innerText = value3
          entry.append(answer1,answer2,answer3)
-         this.div.append(entry)
+         this.div.append(content)
+     }
+     render(){
+         return this.div
      }
  }
