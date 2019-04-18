dc4332c56723ccdaae03c18aaf7a58bd770eaba9
josieyao <yao.josephine@yahoo.com>
2019-04-18 17:32:16 -0500

    Added left and right button function

--- mod3-project-gratitude-journal/gratitudeFrontEnd/src/index.js
+++ mod3-project-gratitude-journal/gratitudeFrontEnd/src/index.js
@@ -15,8 +15,60 @@ document.addEventListener('DOMContentLoaded', () => {
     const userInput2 = document.querySelector('#input2')
     const userInput3 = document.querySelector('#input3')
 
+    
+    const rightBtn = document.querySelector(".arrow.right")
+    const leftBtn = document.querySelector(".arrow.left")
+    const mainContainer = document.querySelector('.main-form-container')
+    const addFormContainer = document.querySelector('.gratitude-form')
+    const meditationContainer = document.querySelector('.meditation-container')
+    const journalsContainer = document.querySelector('.gratitude-journal')
+    const chatContainer = document.querySelector('.chatbox')
+
+    //on load, site will only show "add entry" form
+    meditationContainer.style.display = "none"
+    journalsContainer.style.display = "none"
+    chatContainer.style.display = "none"
+
+    //"right button" event listener
+    
+    let i = 0
+    rightBtn.addEventListener('click', (e) => {
+        e.preventDefault()
+        i++
+        if (i % 2 == 0){
+            addFormContainer.style.display = "block"
+            journalsContainer.style.display = "none"
+            chatContainer.style.display = "none"
+            meditationContainer.style.display = "none"
+        } else {
+            addFormContainer.style.display = "none"
+            journalsContainer.style.display = "none"
+            chatContainer.style.display = "none"
+            meditationContainer.style.display = "block"
+        }
+    })
+
+    //"left button" event listener
+    leftBtn.addEventListener('click', (e) => {
+        e.preventDefault()
+        i++
+        if (i % 2 == 0){
+            addFormContainer.style.display = "block"
+            chatContainer.style.display = "none"
+            meditationContainer.style.display = "none"
+            journalsContainer.style.display = "none"
+        } else {
+            addFormContainer.style.display = "none"
+            chatContainer.style.display = "none"
+            meditationContainer.style.display = "none"
+            journalsContainer.style.display = "block" 
+        }
+    })
+
+    //fetching all journal entries to the page
     Entry.fetchAllEntries()
 
+    //"add a journal entry" eventlistener
     form.addEventListener('submit', (e) => {
         e.preventDefault()
         const newChat = {
@@ -31,23 +83,23 @@ document.addEventListener('DOMContentLoaded', () => {
     })
 
     fetch('http://localhost:3000/entries')
-    .then((res) => res.json())
-    .then((entry) => {
-       entry.forEach(newEntry => {
-        let myEntry = new Entry('gratitude',newEntry.input1, newEntry.input2,newEntry.input3, newEntry.date)
-        myEntry.render(newEntry.id)
-       });
-    })
-
-    
-   fetch('http://localhost:3000/chats')
-    .then((res) => res.json())
-    .then((messages)=>{
-        messages.forEach(message =>{
-            let newMess = new Chat(message.message)
+        .then((res) => res.json())
+        .then((entry) => {
+            entry.forEach(newEntry => {
+                let myEntry = new Entry('gratitude', newEntry.input1, newEntry.input2, newEntry.input3, newEntry.date)
+                myEntry.render(newEntry.id)
+            });
         })
-   })
-   messageBox.addEventListener('click', (e) => {
+
+
+    fetch('http://localhost:3000/chats')
+        .then((res) => res.json())
+        .then((messages) => {
+            messages.forEach(message => {
+                let newMess = new Chat(message.message)
+            })
+        })
+    messageBox.addEventListener('click', (e) => {
         e.preventDefault()
         const newChat = {
             message: message.value
@@ -61,5 +113,8 @@ document.addEventListener('DOMContentLoaded', () => {
     //             myDiary.deleteAll();
     //             view.displayEntries()
     //         },
+
+
+
 
 })
